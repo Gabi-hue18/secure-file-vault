da9bbb798c4dc321096dad70352c61f9eaa8566d
Shadow_Garden <sawantshivam1812@gmail.com>
2026-05-16 15:54:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -75,9 +75,9 @@ def allowed_file(filename):
     return "." in filename and \
            filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
     try:
-    cur.execute("ALTER TABLE files ADD COLUMN file_size TEXT")
-except sqlite3.OperationalError:
-    pass
+        cur.execute("ALTER TABLE files ADD COLUMN file_size TEXT")
+    except sqlite3.OperationalError:
+        pass
 
 try:
     cur.execute("ALTER TABLE files ADD COLUMN upload_time TEXT")
